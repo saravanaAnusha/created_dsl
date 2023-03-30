0be93e566d573b0740cec913e436d8fcb7ee204b
saravanaAnusha <125149756+saravanaAnusha@users.noreply.github.com>
2023-03-29 23:22:04 -0400

    Update saravana_dsl.groovy

--- saravana_dsl.groovy
+++ saravana_dsl.groovy
@@ -4,7 +4,7 @@
     stages {
         stage('clone Repository') {
             steps {
-            git branch:'master' url: 'https://github.com/saravanaAnusha/created_dsl/blob/master/saravana_dsl.groovy'
+             git branch:'master' url{ ['https://github.com/saravanaAnusha/created_dsl/blob/master/saravana_dsl.groovy']}
             }
         }
     }
